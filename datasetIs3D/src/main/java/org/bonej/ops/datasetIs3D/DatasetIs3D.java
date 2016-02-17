3ac55686cc26e97f36bdfa575bcb37958d5577c4
Richard Domander <rdomander@rvc.ac.uk>
2016-02-17 11:27:05 +0000

    Implement DatasetIs3D

--- datasetIs3D/src/main/java/org/bonej/ops/datasetIs3D/DatasetIs3D.java
+++ datasetIs3D/src/main/java/org/bonej/ops/datasetIs3D/DatasetIs3D.java
@@ -1,11 +1,14 @@
 package org.bonej.ops.datasetIs3D;
 
 import net.imagej.Dataset;
+import net.imagej.axis.CalibratedAxis;
 import net.imagej.ops.Op;
 import net.imagej.ops.OpEnvironment;
 import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+
+import java.util.Arrays;
 
 /**
  * An Op which checks whether the given Dataset has three spatial dimensions
@@ -15,6 +18,8 @@ import org.scijava.plugin.Plugin;
 
 @Plugin(type = Op.class, name = "datasetIs3D")
 public class DatasetIs3D implements Op {
+    private static final int SPATIAL_DIMENSIONS = 3;
+
     @Parameter(type = ItemIO.INPUT)
     private Dataset dataset;
 
@@ -33,6 +38,15 @@ public class DatasetIs3D implements Op {
 
     @Override
     public void run() {
+        final int numDimensions = dataset.numDimensions();
+        if (numDimensions < SPATIAL_DIMENSIONS) {
+            is3D = false;
+            return;
+        }
 
+        final CalibratedAxis[] axes = new CalibratedAxis[numDimensions];
+        dataset.axes(axes);
+        long spatialAxisCount = Arrays.stream(axes).filter(axis -> axis.type().isSpatial()).count();
+        is3D = spatialAxisCount == SPATIAL_DIMENSIONS;
     }
 }
--- datasetIs3D/src/test/java/org/bonej/ops/datasetIs3D/DatasetIs3DTest.java
+++ datasetIs3D/src/test/java/org/bonej/ops/datasetIs3D/DatasetIs3DTest.java
@@ -8,6 +8,7 @@ import net.imglib2.type.numeric.integer.UnsignedByteType;
 import org.junit.AfterClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -23,6 +24,17 @@ public class DatasetIs3DTest {
         ij.context().dispose();
     }
 
+
+    @Test
+    public void TestImageWithLessThanThreeDimensionsFails() {
+        final long[] dims = {100, 100, 10};
+        final AxisType[] axisTypes = {Axes.X, Axes.Y, Axes.TIME};
+        Dataset dataset = ij.dataset().create(new UnsignedByteType(), dims, "Test image", axisTypes);
+
+        boolean result = (boolean) ij.op().run("datasetIs3D", dataset);
+        assertFalse("An image with two spatial dimensions is not 3D", result);
+    }
+
     @Test
     public void Test3DImagePasses() {
         final long[] dims = {100, 100, 100, 3};
@@ -31,5 +43,17 @@ public class DatasetIs3DTest {
 
         boolean result = (boolean) ij.op().run("datasetIs3D", dataset);
         assertTrue("An image with three spatial dimensions is 3D", result);
+    }
+
+
+    @Test
+    public void TestImageWithMoreThanThreeDimensionsFails() {
+        final AxisType W = Axes.get("W", true);
+        final long[] dims = {100, 100, 100, 100};
+        final AxisType[] axisTypes = {Axes.X, Axes.Y, Axes.Z, W};
+        Dataset dataset = ij.dataset().create(new UnsignedByteType(), dims, "Test image", axisTypes);
+
+        boolean result = (boolean) ij.op().run("datasetIs3D", dataset);
+        assertFalse("An image with four spatial dimensions is not 3D", result);
     }
 }
